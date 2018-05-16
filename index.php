2bd3c7aaac6db0dd2aa3dadd4608759f8b426d9a
Erol Bakic <erol.bakic@hotmail.com>
2018-05-16 12:22:54 +0200

    tilføjede footer, opdaterede forside hero og andre små fikseri

--- index.php
+++ index.php
@@ -33,6 +33,33 @@
     </header>
 
     <?php require_once('pages/page-' . navDir($nav, true) . '.html'); ?>
+
+    <footer class="main-footer">
+      <div class="main-footer-content">
+
+        <div class="footer-col-adress">
+          <h3 class="footer-title">Kontakt Billum Friskole</h3>
+          <ul>
+            <li>Telefon: <a href="tel:+4551889596">+45 51 88 95 96</a></li>
+            <li>Mail: <a href="mailto:gimo@billumfriskole.dk">gimo@BillumFriskole.dk</a></li>
+            <li><address>Adresse: <a target="_blank" href="https://www.google.dk/maps?q=Kildegårsvej-20-6852-Billum">Kildegårsvej 20, 6852 Billum</a></address></li>
+          </ul>
+        </div>
+
+        <div class="footer-col-intranet">
+          <h3 class="footer-title">Intranet</h3>
+          <p>Log ind på intranettet her: <a href="https://billumfriskole.viggo.dk" target="_blank">Viggo.dk</a></p>
+        </div>
+
+        <div class="footer-col-social">
+          <h3 class="footer-title">Sociale Medier</h3>
+          <ul>
+            <li><a href="https://www.facebook.com/billumfriskole/" target="_blank"><img src="img/gfx/facebook.png" alt="Facebook Ikon">Facebook</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </footer>
   </div>
 </body>
 </html>
